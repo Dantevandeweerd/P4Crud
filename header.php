47b2eb169856658ef16b25fa343fdeffea3cf3d5
Danteboy141 <dantevdweerd@gmail.com>
2024-05-15 08:54:48 +0200

    Header gemaakt en af

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/Style/style.css" />
+  </head>
+  <body>
+    <nav>
+      <img src="assets/img/logo.png" id="header-logo" alt="logo" />
+      <img src="assets/img/vliegtuig.png" alt="vliegtuig" />
+      <div class="header-buttons">
+        <button>Locaties</button>
+        <button>Over</button>
+        <button>Contact</button>
+        <button>Mijn D&A</button>
+      </div>
+    </nav>
+  </body>
+</html>
