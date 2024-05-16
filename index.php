1830bb0a78a6372b5e37e1e10169bd768293ca50
Danteboy141 <dantevdweerd@gmail.com>
2024-05-16 13:31:38 +0200

    landingpage gerealiseert

--- index.php
+++ index.php
@@ -5,14 +5,148 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
     <link rel="stylesheet" href="assets/style/style.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+      rel="stylesheet" />
   </head>
   <body>
     <main>
       <?php include 'header.php'?>
-      
-      <?php include 'footer.php'?>
+      <section>
+        <div>
+          <img
+            src="assets/img/landingpagefoto.png"
+            alt="landingpagefoto"
+            id="landingpagefoto" />
+          <form action="" class="boeking">
+            <input type="text" placeholder="Bestemming" />
+            <input type="date" name="" id="" />
+            <input type="date" name="" id="" />
+            <input type="text" placeholder="kamer(s), Reizigers" />
+            <input type="text" placeholder="Luchthaven" />
+            <input
+              type="submit"
+              value="Bekijk resultaten"
+              id="boeking-resultaat" />
+          </form>
+        </div>
+      </section>
 
+      <section class="vakantie-locaties">
+        <div class="hotel">
+          <div class="boeking-blok">
+            <div class="vakantie-foto">
+              <img src="assets/img/doryssa.png" alt="doryssa" />
+            </div>
+            <div class="vakantie-naam">
+              <img src="assets/img/4ster.png" alt="4ster" />
+              <h2>Appartementen Doryssa Coast</h2>
+              <p class="vakantie-bestemming">Griekenland Samos Pythagorion</p>
+              <div class="kosten">
+                <p>
+                  vanafprijs p.p
+                  <img id="info" src="assets/img/info.png" alt="info" />
+                </p>
+                <p class="prijs">€ 865</p>
+                <p class="verborgen-kosten">*Geen verborgen kosten</p>
+              </div>
+            </div>
+          </div>
+          <div class="boeking-blok">
+            <div class="vakantie-foto">
+              <img src="assets/img/doryssa.png" alt="doryssa" />
+            </div>
+            <div class="vakantie-naam">
+              <img src="assets/img/4ster.png" alt="4ster" />
+              <h2>Appartementen Doryssa Coast</h2>
+              <p class="vakantie-bestemming">Griekenland Samos Pythagorion</p>
+              <div class="kosten">
+                <p>
+                  vanafprijs p.p
+                  <img id="info" src="assets/img/info.png" alt="info" />
+                </p>
+                <p class="prijs">€ 865</p>
+                <p class="verborgen-kosten">*Geen verborgen kosten</p>
+              </div>
+            </div>
+          </div>
+          <div class="boeking-blok">
+            <div class="vakantie-foto">
+              <img src="assets/img/doryssa.png" alt="doryssa" />
+            </div>
+            <div class="vakantie-naam">
+              <img src="assets/img/4ster.png" alt="4ster" />
+              <h2>Appartementen Doryssa Coast</h2>
+              <p class="vakantie-bestemming">Griekenland Samos Pythagorion</p>
+              <div class="kosten">
+                <p>
+                  vanafprijs p.p
+                  <img id="info" src="assets/img/info.png" alt="info" />
+                </p>
+                <p class="prijs">€ 865</p>
+                <p class="verborgen-kosten">*Geen verborgen kosten</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="hotel">
+          <div class="boeking-blok">
+            <div class="vakantie-foto">
+              <img src="assets/img/doryssa.png" alt="doryssa" />
+            </div>
+            <div class="vakantie-naam">
+              <img src="assets/img/4ster.png" alt="4ster" />
+              <h2>Appartementen Doryssa Coast</h2>
+              <p class="vakantie-bestemming">Griekenland Samos Pythagorion</p>
+              <div class="kosten">
+                <p>
+                  vanafprijs p.p
+                  <img id="info" src="assets/img/info.png" alt="info" />
+                </p>
+                <p class="prijs">€ 865</p>
+                <p class="verborgen-kosten">*Geen verborgen kosten</p>
+              </div>
+            </div>
+          </div>
+          <div class="boeking-blok">
+            <div class="vakantie-foto">
+              <img src="assets/img/doryssa.png" alt="doryssa" />
+            </div>
+            <div class="vakantie-naam">
+              <img src="assets/img/4ster.png" alt="4ster" />
+              <h2>Appartementen Doryssa Coast</h2>
+              <p class="vakantie-bestemming">Griekenland Samos Pythagorion</p>
+              <div class="kosten">
+                <p>
+                  vanafprijs p.p
+                  <img id="info" src="assets/img/info.png" alt="info" />
+                </p>
+                <p class="prijs">€ 865</p>
+                <p class="verborgen-kosten">*Geen verborgen kosten</p>
+              </div>
+            </div>
+          </div>
+          <div class="boeking-blok">
+            <div class="vakantie-foto">
+              <img src="assets/img/doryssa.png" alt="doryssa" />
+            </div>
+            <div class="vakantie-naam">
+              <img src="assets/img/4ster.png" alt="4ster" />
+              <h2>Appartementen Doryssa Coast</h2>
+              <p class="vakantie-bestemming">Griekenland Samos Pythagorion</p>
+              <div class="kosten">
+                <p>
+                  vanafprijs p.p
+                  <img id="info" src="assets/img/info.png" alt="info" />
+                </p>
+                <p class="prijs">€ 865</p>
+                <p class="verborgen-kosten">*Geen verborgen kosten</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <?php include 'footer.php'?>
     </main>
   </body>
 </html>
