1613a943306d49b8251df2f87bae7672f9662586
Danteboy141 <dantevdweerd@gmail.com>
2024-05-17 16:04:18 +0200

    start gemaakt aan het responsive maken van landingpage

--- header.php
+++ header.php
@@ -11,13 +11,25 @@
       <a href="index.php"
         ><img src="assets/img/logo.png" id="header-logo" alt="logo"
       /></a>
-      <img src="assets/img/vliegtuig.png" alt="vliegtuig" />
+      <img src="assets/img/vliegtuig.png" alt="vliegtuig" id="vliegtuig" />
       <div class="header-buttons">
         <a href=""><button>Locaties</button></a>
         <a href=""><button>Over</button></a>
         <a href=""><button>Contact</button></a>
         <a href=""><button>Mijn D&A</button></a>
       </div>
+      <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href=""><button>Locaties</button></a>
+        <a href=""><button>Over</button></a>
+        <a href=""><button>Contact test</button></a>
+        <a href=""><button>Mijn D&A</button></a>
+      </div>
+      <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+
+      <script src="assets/javascript/script.js"></script>
+
+
     </nav>
   </body>
 </html>
