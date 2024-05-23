4ce0e2b77637eec3d86c6ae7a8fd036e5b3f9891
Danteboy141 <dantevdweerd@gmail.com>
2024-05-23 13:26:01 +0200

    inlog pagina gerealiseert

--- mijnd&aregister.php
+++ mijnd&aregister.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="assets/style/style.css" />
     <link
       href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
-      rel="stylesheet" />
+      rel="stylesheet"
+    />
   </head>
   <body>
     <main>
@@ -15,7 +16,8 @@
       <img
         src="assets/img/achtergrond.png"
         alt="achtergrond"
-        id="inlog-achtergrond" />
+        id="inlog-achtergrond"
+      />
       <div class="inlog-container">
         <div id="inlog-form">
           <img src="assets/img/logolos.png" alt="logolos" id="inlog-logo" />
@@ -27,19 +29,11 @@
           </h2>
           <form action="" class="register-form">
             <input type="text" id="email" placeholder="E-mailadres" />
-            <input type="password" id="wachtwoord" placeholder="Wachtwoord" />
+            <input type="password" id="wachtword" placeholder="Wachtwoord" />
+            <h3>Wachtword vergeten</h3>
+            <input type="submit" id="versturen" placeholder="inloggen" />
+            <h3>geen account? Creëer een account</h3>
           </form>
-          <div class="wachtwoord-vergeten">
-            <button><h3>Wachtword vergeten</h3></button>
-          </div>
-          <div class="inloggen">
-            <input type="submit" id="versturen" value="Inloggen" />
-          </div>
-          <div id="register-link">
-            <a href="mijnd&aregister.php"
-              ><button>geen account? <span id="Creëer">Creëer een account</span></button></a
-            >
-          </div>
         </div>
       </div>
       <?php include 'footer.php'?>
